3017e018ba627007cb270c7c7e4fdd100f120718
PikuzaMarie <pikuza.mari@gmail.com>
2024-10-14 16:22:37 +0300

    feat: bind table zjblessons_base_Headers by js, select only display fields and HeaderID, add descending sort by DocumentDate

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -3,9 +3,10 @@ sap.ui.define([
 		"zjblessons/Worklist/controller/BaseController",
 		"sap/ui/model/json/JSONModel",
 		"zjblessons/Worklist/model/formatter",
+		"sap/ui/model/Sorter",
 		"sap/ui/model/Filter",
 		"sap/ui/model/FilterOperator"
-	], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+	], function (BaseController, JSONModel, formatter, Sorter, Filter, FilterOperator) {
 		"use strict";
 
 		return BaseController.extend("zjblessons.Worklist.controller.Worklist", {
@@ -19,6 +20,59 @@ sap.ui.define([
 
 				this.setModel(oViewModel, "worklistView");
 
+			},
+
+			/**
+			 * @override
+			 */
+			onBeforeRendering: function() {
+				this._bindTable();
+			},
+
+			_bindTable() {
+				const oTable = this.getView().byId('table');
+
+				oTable.bindItems({
+					path: '/zjblessons_base_Headers',
+					sorter: [
+						new Sorter('DocumentDate', true)
+					],
+					template: this._getTableTemplate(),
+					urlParameters: {
+						$select:'HeaderID,DocumentNumber,DocumentDate,PlantText,RegionText,Description,Created'
+					}
+
+				});
+
+			},
+
+			_getTableTemplate() {
+				const oTemplate = new sap.m.ColumnListItem({
+					type: 'Navigation',
+					navigated: true,
+					cells: [
+						new sap.m.Text({
+							text: '{DocumentNumber}'
+						}),
+						new sap.m.Text({
+							text: '{DocumentDate}'
+						}),
+						new sap.m.Text({
+							text: '{PlantText}'
+						}),
+						new sap.m.Text({
+							text: '{RegionText}'
+						}),
+						new sap.m.Text({
+							text: '{Description}'
+						}),
+						new sap.m.Text({
+							text: '{Created}'
+						}),
+					]
+				});
+
+				return oTemplate;
 			},
 
 			onSearchDocNum(oEvent) {

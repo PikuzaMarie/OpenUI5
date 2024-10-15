6caf2902b680ae03508e0a36ffc0917d4c8cc0d0
PikuzaMarie <pikuza.mari@gmail.com>
2024-10-15 21:57:24 +0300

    refactor: delete unnecessary comments, models.js, test and localService folders

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -4,12 +4,6 @@ sap.ui.define([
 
 		return {
 
-			/**
-			 * Rounds the number unit value to 2 digits
-			 * @public
-			 * @param {string} sValue the number string to be rounded
-			 * @returns {string} sValue with 2 digits rounded
-			 */
 			numberUnit : function (sValue) {
 				if (!sValue) {
 					return "";

c6183c61463d2fc74feb72be5f6e650396229721
PikuzaMarie <pikuza.mari@gmail.com>
2024-10-22 14:22:54 +0300

    feat: add functions for edit and cancel buttons

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -3,12 +3,14 @@ sap.ui.define([
 		"zjblessons/Worklist/controller/BaseController",
 		"sap/ui/model/json/JSONModel",
 		"sap/ui/core/routing/History",
-		"zjblessons/Worklist/model/formatter"
+		"zjblessons/Worklist/model/formatter",
+		"sap/m/MessageToast"
 	], function (
 		BaseController,
-		JSONModel,
-		History,
-		formatter
+	JSONModel,
+	History,
+	formatter,
+	MessageToast
 	) {
 		"use strict";
 
@@ -16,22 +18,13 @@ sap.ui.define([
 
 			formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
-
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
 			onInit : function () {
-				// Model used to manipulate control states. The chosen values make sure,
-				// detail page is busy indication immediately so there is no break in
-				// between the busy indication for loading the view's meta data
 				var iOriginalBusyDelay,
 					oViewModel = new JSONModel({
 						busy : true,
-						delay : 0
+						delay : 0,
+						isEditMode: false,
+						sSelectedTab: "List"
 					});
 
 				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
@@ -40,23 +33,11 @@ sap.ui.define([
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 				this.setModel(oViewModel, "objectView");
 				this.getOwnerComponent().getModel().metadataLoaded().then(function () {
-						// Restore original busy indicator delay for the object view
 						oViewModel.setProperty("/delay", iOriginalBusyDelay);
 					}
 				);
 			},
 
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
-
-
-			/**
-			 * Event handler  for navigating back.
-			 * It there is a history entry we go one step back in the browser history
-			 * If not, it will replace the current entry of the browser history with the worklist route.
-			 * @public
-			 */
 			onNavBack : function() {
 				var sPreviousHash = History.getInstance().getPreviousHash();
 
@@ -67,16 +48,6 @@ sap.ui.define([
 				}
 			},
 
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
-
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-			 * @private
-			 */
 			_onObjectMatched : function (oEvent) {
 				var sObjectId =  oEvent.getParameter("arguments").objectId;
 				this.getModel().metadataLoaded().then( function() {
@@ -87,12 +58,6 @@ sap.ui.define([
 				}.bind(this));
 			},
 
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {string} sObjectPath path to the object to be bound
-			 * @private
-			 */
 			_bindView : function (sObjectPath) {
 				var oViewModel = this.getModel("objectView"),
 					oDataModel = this.getModel();
@@ -103,10 +68,6 @@ sap.ui.define([
 						change: this._onBindingChange.bind(this),
 						dataRequested: function () {
 							oDataModel.metadataLoaded().then(function () {
-								// Busy indicator on view should only be set if metadata is loaded,
-								// otherwise there may be two busy indications next to each other on the
-								// screen. This happens because route matched handler already calls '_bindView'
-								// while metadata is loaded.
 								oViewModel.setProperty("/busy", true);
 							});
 						},
@@ -127,20 +88,19 @@ sap.ui.define([
 					this.getRouter().getTargets().display("objectNotFound");
 					return;
 				}
-
-				var oResourceBundle = this.getResourceBundle(),
-					oObject = oView.getBindingContext().getObject(),
-					sObjectId = oObject.HeaderID,
-					sObjectName = oObject.CreatedBy;
-
 				oViewModel.setProperty("/busy", false);
+			},
 
-				oViewModel.setProperty("/shareSendEmailSubject",
-				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-				oViewModel.setProperty("/shareSendEmailMessage",
-				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
-			}
+			onEdit() {
+				this.getView().getModel("objectView").setProperty('/isEditMode', true);
+			},
 
+			onCancel() {
+				const oViewModel = this.getView().getModel("objectView");
+				oViewModel.setProperty("/isEditMode", false);
+				this.getModel().resetChanges();
+			}			
+			
 		});
 
 	}
